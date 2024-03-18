fd5ac5c7f1626d96bb6c94b8256906f2a8f2d7d6
temmydhayor <ttemitopealao@gmail.com>
2024-03-18 14:22:24 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,116 +1,80 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This is the file storage class for AirBnB"""
 import json
-import os
-import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+import shlex
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
     Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+        __file_path: path to the JSON file
+        __objects: objects will be stored
     """
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """Return the dictionary __objects."""
-        return self.__objects
+    def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        objname = obj.__class__.__name__
-        objID = obj.id
-        key = f"{objname}.{objID}"  # <class name>.id = obj
-        self.__objects[key] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
+        """serialize the file path to JSON file path
         """
-            Serialize __objects to the JSON file __file_path.
-            serialize the object by first converting it to a dictionary
-        """
-        object_dict = {}
-
-        for key in self.__objects.keys():
-            if type(self.__objects[key]) != dict:
-                object_dict[key] = self.__objects[key].to_dict()
-        """ convert the dictionary object to json and write to the file"""
-        file_name = self.__file_path
-        with open(file_name, "w", encoding="utf-8") as jsonfile:
-            """# json.dump(object_dict, jsonfile)"""
-            jsonfile.write(json.dumps(object_dict))
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references."""
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review
-                   }
-        return classes
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Reloads the stored objects"""
+        """serialize the file path to JSON file path
+        """
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
+            pass
 
-        if os.path.exists(FileStorage.__file_path):
-            """load the file and dump content as dictionary"""
-            with open(FileStorage.__file_path, "r", encoding="utf-8") \
-                    as my_file:
-                object_dict = json.loads(my_file.read())
-            final_dict = {}
+    def delete(self, obj=None):
+        """ delete an existing element
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
-            for id, dictionary in object_dict.items():
-                class_name = dictionary['__class__']
-                final_dict[id] = self.classes()[class_name](**dictionary)
-            FileStorage.__objects = final_dict
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname."""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+    def close(self):
+        """ calls reload()
+        """
+        self.reload()
