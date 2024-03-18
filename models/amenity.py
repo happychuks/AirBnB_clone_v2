4aaa914c2a2b4ec3d9f1db3a99f24b48b8e232f1
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-03-18 14:38:00 -0700

    refactored the test files to cover all necessary test cases

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """Defines the Amenity class."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Represent an amenity.
 
     Attributes:
         name (str): The name of the amenity.
     """
 
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
